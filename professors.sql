b8c5aefe650b902b48916e280256b9afe1c60798
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2021-12-11 09:13:56 +0100

    Update professors.sql

--- professors.sql
+++ professors.sql
@@ -1,3 +1,8 @@
+-- Calculate the net amount as amount + fee
+SELECT transaction_date, amount + CAST(fee AS integer) AS net_amount 
+FROM transactions;
+
+--
 ALTER TABLE professors 
 ALTER COLUMN firstname 
 TYPE varchar(16)
