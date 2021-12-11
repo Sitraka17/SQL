c4d3ce5ce9e77643f2dcb5b2c5544c6a71f2421f
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2021-12-11 22:13:10 +0100

    Update professors.sql

--- professors.sql
+++ professors.sql
@@ -7,3 +7,8 @@ ALTER TABLE professors
 ALTER COLUMN firstname 
 TYPE varchar(16)
 USING SUBSTRING(firstname FROM 1 FOR 16)
+
+
+-- Make universities.university_shortname unique
+ALTER TABLE universities
+ADD CONSTRAINT university_shortname_unq UNIQUE(university_shortname);
