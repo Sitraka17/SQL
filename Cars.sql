2d5bd4288d609a9c4296548e53d99ec0e74437b8
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2021-12-20 22:06:09 +0100

    Create Cars.sql

---
+++ Cars.sql
@@ -0,0 +1,23 @@
+CREATE TABLE cars (
+ make varchar(64) NOT NULL,
+ model varchar(64) NOT NULL,
+ mpg integer NOT NULL
+)
+-- Count the number of distinct rows with columns make, model
+SELECT COUNT(DISTINCT(make, model)) 
+FROM cars;
+
+-- Add the id column
+ALTER TABLE cars
+ADD COLUMN id varchar(128);
+
+-- Update id with make + model
+UPDATE cars
+SET id = CONCAT(make, model);
+
+-- Make id a primary key
+ALTER TABLE cars
+ADD CONSTRAINT id_pk PRIMARY KEY(id);
+
+-- Have a look at the table
+SELECT * FROM cars;
