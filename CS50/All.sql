3f9fdee891a5cd2c8fc79b260e857afc62129fb1
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-11-27 22:37:07 +0100

    Update All.sql

--- CS50/All.sql
+++ CS50/All.sql
@@ -9,3 +9,4 @@ FROM songs;
 --Your query should output a table with a single column for the name of each song.
 
 SELECT name FROM songs ORDER BY tempo;
+SELECT name FROM songs ORDER BY duration_ms DESC LIMIT 5;
