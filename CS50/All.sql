1b0937c249d0e9c76d7af43cda3edb379f152daa
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-12-17 13:40:29 +0100

    Update All.sql

--- CS50/All.sql
+++ CS50/All.sql
@@ -10,3 +10,12 @@ FROM songs;
 
 SELECT name FROM songs ORDER BY tempo;
 SELECT name FROM songs ORDER BY duration_ms DESC LIMIT 5;
+
+
+SELECT distinct(name) FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.title IN(SELECT distinct(movies.title) FROM people
+JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id
+WHERE people.name = "Kevin Bacon" AND people.birth = 1958) AND people.name != "Kevin Bacon";
