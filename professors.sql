4c415b1f19d198a8c817f41c10f6230e4412118a
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-07 21:41:03 +0100

    Update professors.sql

--- professors.sql
+++ professors.sql
@@ -1,3 +1,6 @@
+-- If you find it thanks to a search engine, you know why you are here haha. 
+--GL & try to improve your skills 
+
 -- Calculate the net amount as amount + fee
 SELECT transaction_date, amount + CAST(fee AS integer) AS net_amount 
 FROM transactions;
@@ -48,3 +51,15 @@ ADD CONSTRAINT university_pk PRIMARY KEY (id);
 ALTER TABLE professors 
 ADD COLUMN id serial;
 
+
+-- Add a professor_id column
+ALTER TABLE affiliations
+ADD COLUMN professor_id integer REFERENCES professors (id);
+
+-- Rename the organization column to organization_id
+ALTER TABLE affiliations
+RENAME organization TO organization_id;
+
+-- Add a foreign key on organization_id
+ALTER TABLE affiliations
+ADD CONSTRAINT affiliations_organization_fkey FOREIGN KEY (organization_id) REFERENCES organizations (id);
