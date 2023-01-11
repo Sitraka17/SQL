4ac565ef8571e9e17f0b712f437080caf0c6e20b
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2023-01-11 10:23:49 +0100

    Update professors.sql

--- professors.sql
+++ professors.sql
@@ -73,3 +73,10 @@ WHERE affiliations.firstname = professors.firstname AND affiliations.lastname = 
 
 -- Have a look at the 10 first rows of affiliations again
 SELECT * FROM affiliations LIMIT 10;
+
+
+
+-- Identify the correct constraint name
+SELECT constraint_name, table_name, constraint_type
+FROM information_schema.table_constraints
+WHERE constraint_type = 'FOREIGN KEY';
